f00d504f125057ef11f168efe0c0276a6094648b
vgoel <59496579+goelvaibhav105@users.noreply.github.com>
2022-11-25 16:14:38 +0530

    Show vechile func

--- src/components/VehicleList.js
+++ src/components/VehicleList.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
+import Home from './Home';
+import VechileCards from './VechileCards';
 
-export default function VehicleList() {
+export default function VehicleList({userDetail}) {
+    const { token } = userDetail;
+    const [allData, setData] = useState(null);
+    const [userData, setUserData] = useState(null);
+  
+    useEffect(() => {
+      getVehicleList();
+    }, []);
+  
+    const getVehicleList = () => {
+      fetch(" https://staging-api.tracknerd.io/v1/vehicle-groups/vehicles", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((response) => response.json())
+        .then(({ data }) => {
+          setData([...data]);
+          setUserData([data[0]])
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    };
+
   return (
     <div>
-      Vechile
+      <Home userData={userData}/>
+      <VechileCards userData={userData}/>
     </div>
   )
 }
---
+++ src/components/firebase/firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDahAZLP3Fe68j9C-7ZT9i0nAqiQWXhs_4",
+  authDomain: "http://tracknerd-staging.firebaseapp.com/",
+  databaseURL: "https://tracknerd-staging-default-rtdb.firebaseio.com/",
+  storageBucket: "tracknerd-staging.appspot.com",
+  appId: "1:847967007196:web:ae4df284f5560af4139f19"
+};
+
+export default  app = initializeApp(firebaseConfig);
